edc5a51fcf52c62363e018a55a9cb30f83f7ef26
“Shady <shady.kahaleh@gmail.com>
2020-07-26 21:47:02 +0300

    Initial Commit

---
+++ TvShow/TvShow.h
@@ -0,0 +1,18 @@
+//
+//  TvShow.h
+//  TvShow
+//
+//  Created by Shady Kahaleh on 7/26/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TvShow.
+FOUNDATION_EXPORT double TvShowVersionNumber;
+
+//! Project version string for TvShow.
+FOUNDATION_EXPORT const unsigned char TvShowVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TvShow/PublicHeader.h>
+
+
