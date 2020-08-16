2216df233092fc250c5ae60d2da17cced3d04234
“Shady <shady.kahaleh@gmail.com>
2020-08-16 15:18:39 +0300

    add new properties for tv show

--- TvShows/TvShow.swift
+++ TvShows/TvShow.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-enum Genre{
+public enum Genre {
     case comedy, action
 }
 
@@ -15,5 +15,42 @@ public struct TvShow : Equatable{
     var id : UUID
     var name: String
     var language: String
-    var genres : [Genre]
+    var genres : [String]?
+    var status : String
+    var message : String?
+    var code : Int?
+
+    public init(id : UUID,name: String, language: String, status: String, genres : [String]? = nil){
+        self.id = id
+        self.name = name
+        self.language = language
+        self.status = status
+        self.genres = genres
+    }
 }
+
+extension TvShow : Decodable{
+    private enum CodingKeys : String, CodingKey{
+        case id
+        case name
+        case status
+        case language
+        case genres
+        case message
+        case code
+    }
+}
+
+
+
+public struct ErrorObject : Decodable{
+    var status : Int
+    var message : String
+    var code : Int
+    var name : String
+}
+
+
+
+
+
