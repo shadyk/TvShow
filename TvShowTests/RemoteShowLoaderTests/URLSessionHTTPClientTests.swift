1dfc8e6068ff7fe46112c96b2497bb0dd23282fe
“Shady <shady.kahaleh@gmail.com>
2020-09-05 16:45:16 +0300

    add a test to test url correctness in it own  test case

--- TvShowTests/RemoteShowLoaderTests/URLSessionHTTPClientTests.swift
+++ TvShowTests/RemoteShowLoaderTests/URLSessionHTTPClientTests.swift
@@ -24,6 +24,20 @@ class URLSessionHTTPClient {
 
 class URLSessionHTTPClientTests: XCTestCase {
 
+    func test_getFromURL_performsGETrequest(){
+        URLProtocolStub.startInterceptingRequests()
+        let url = URL(string: "http://any-url.com")!
+        let exp = expectation(description: "Wait for completion")
+        URLProtocolStub.observeRequests{ request in
+            XCTAssertEqual(request.url, url)
+            XCTAssertEqual(request.httpMethod, "GET")
+            exp.fulfill()
+        }
+        URLSessionHTTPClient().get(from: url, completion: {_ in })
+        waitForExpectations(timeout: 0.1)
+        URLProtocolStub.stopInterceptingRequests()
+    }
+
     func test_getFromURL_failsOnRequestError() {
 
         URLProtocolStub.startInterceptingRequests()
@@ -54,6 +68,7 @@ class URLSessionHTTPClientTests: XCTestCase {
 
     private class URLProtocolStub: URLProtocol {
         private static var stub : Stub?
+        private static var requestObserver : ((URLRequest)->Void)?
 
         private struct Stub {
             let data: Data?
@@ -72,14 +87,20 @@ class URLSessionHTTPClientTests: XCTestCase {
         static func stopInterceptingRequests() {
             URLProtocol.unregisterClass(URLProtocolStub.self)
             stub = nil
+            requestObserver = nil
         }
 
         override class func canInit(with request: URLRequest) -> Bool {
+            requestObserver?(request)
             return true
         }
 
         override class func canonicalRequest(for request: URLRequest) -> URLRequest {
             return request
+        }
+
+        static func observeRequests(observer: @escaping (URLRequest)->Void){
+            requestObserver = observer
         }
 
         override func startLoading() {
